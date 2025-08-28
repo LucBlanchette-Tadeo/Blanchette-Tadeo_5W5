5cf4d0b4dff3d8b2478664393ad8a2358de1c812
LucBlanchette-Tadeo <2234344@cegepmontpetit.ca>
2025-08-28 09:19:21 -0400

    Clear

--- Models/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVCEtWebAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
